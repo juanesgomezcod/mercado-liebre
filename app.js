97827bc2a1d2b00cd6b03567a0060e345222f22e
juanesgomezcod <juanesgomez@gmail.com>
2022-10-14 09:19:23 -0500

    vistas registro y login

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({extended: false}));
 
-app.listen(process.env.PORT || 3000,() => console.log("servidor corriendo en el puerto 3000"));
+app.listen(process.env.PORT || 3030,() => console.log("servidor corriendo en el puerto 3030"));
 
 app.get("/", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/home.html"));
+    res.sendFile(path.resolve(__dirname, "src/views/home.html"));
 });
 
 
 
 app.get("/register", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/register.html"));
+    res.sendFile(path.resolve(__dirname, "src/views/register.html"));
 });
 
 app.post('/register', (req,res)=>{ 
